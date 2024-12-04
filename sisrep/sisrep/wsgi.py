9ed87c9995d617be816621de7a251b478735d983
AdairGV10 <90804160+AdairGV10@users.noreply.github.com>
2024-12-04 11:51:29 -0600

    Update wsgi.py

--- sisrep/sisrep/wsgi.py
+++ sisrep/sisrep/wsgi.py
@@ -8,8 +8,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sisrep.sisrep.settings')
+
+# Intentar importar core
+try:
+    import core
+except ModuleNotFoundError as e:
+    print(f"Error al importar 'core': {e}")
+    sys.exit(1)
 
 application = get_wsgi_application()
