ba72c0bfc583c41276e1494347c56f8b6199d298
AdairGV10 <90804160+AdairGV10@users.noreply.github.com>
2024-12-04 13:00:50 -0600

    Update wsgi.py

--- sisrep/sisrep/wsgi.py
+++ sisrep/sisrep/wsgi.py
@@ -8,19 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
 
-# Agregar el directorio al PYTHONPATH
-sys.path.append(os.path.join(os.path.dirname(__file__), 'sisrep'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sisrep.sisrep.settings')
 
-# Intentar importar core
-try:
-    import core
-except ModuleNotFoundError as e:
-    print(f"Error al importar 'core': {e}")
-    sys.exit(1)
 
 application = get_wsgi_application()
