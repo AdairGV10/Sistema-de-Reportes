dbcc5276cd001197800174682731471d28437960
AdairGV10 <90804160+AdairGV10@users.noreply.github.com>
2024-12-04 13:55:35 -0600

    cambios path sisrep.wsgi

--- sisrep/sisrep/wsgi.py
+++ sisrep/sisrep/wsgi.py
@@ -8,17 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
 
-# Depuración: Mostrar las rutas de búsqueda de módulos
-print("Rutas de búsqueda de módulos:")
-print(sys.path)
-
-# Añadir la ruta del proyecto al sys.path para asegurarnos de que 'core' sea accesible
-sys.path.append(os.path.join(os.path.dirname(__file__), 'sisrep'))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sisrep.sisrep.settings')
+# Establece el módulo de configuración de Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sisrep.settings')
 
 application = get_wsgi_application()
 
