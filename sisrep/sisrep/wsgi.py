0c1820934bea0364b7013e62fcd15a35f3d5dc0a
AdairGV10 <90804160+AdairGV10@users.noreply.github.com>
2024-12-04 14:42:05 -0600

    version 2

--- sisrep/sisrep/wsgi.py
+++ sisrep/sisrep/wsgi.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Establece el módulo de configuración de Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sisrep.sisrep.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sisrep.settings')
 
 application = get_wsgi_application()
 
