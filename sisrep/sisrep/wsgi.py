57991089d6c3c4faf128e45db60d7ba57e236ed6
AdairGV10 <90804160+AdairGV10@users.noreply.github.com>
2024-12-04 13:20:35 -0600

    se añadio un test de core

--- sisrep/sisrep/wsgi.py
+++ sisrep/sisrep/wsgi.py
@@ -8,10 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
+# Depuración: Mostrar las rutas de búsqueda de módulos
+print("Rutas de búsqueda de módulos:")
+print(sys.path)
+
+# Añadir la ruta del proyecto al sys.path para asegurarnos de que 'core' sea accesible
+sys.path.append(os.path.join(os.path.dirname(__file__), 'sisrep'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sisrep.sisrep.settings')
 
-
 application = get_wsgi_application()
+
