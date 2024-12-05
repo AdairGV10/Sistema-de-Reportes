1daefefba3aa13e8c26aa931969bb3d07eec7fab
AdairGV10 <90804160+AdairGV10@users.noreply.github.com>
2024-12-04 19:44:35 -0600

    arreglos

--- sisrep/sisrep/wsgi.py
+++ sisrep/sisrep/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sisrep.sisrep.settings')
+
+# Prueba de importación
+try:
+    import sisrep.sisrep.settings
+    print("Settings module loaded successfully!")
+except ModuleNotFoundError as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sisrep.settings')
-
 application = get_wsgi_application()
+
 
